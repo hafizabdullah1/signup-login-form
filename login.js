bde554a2d2767302c800a6b5b56a799c83cd37f4
hafiz abdullah <habdullah4510@gmail.com>
2025-07-14 12:30:33 +0500

    fix some issues

--- login.js
+++ login.js
@@ -1,27 +1,32 @@
 const alluser  = JSON.parse(localStorage.getItem("data")) || [];
-const previous = JSON.parse(localStorage.getItem("previous")) || [];
+// const previous = JSON.parse(localStorage.getItem("previous")) || [];
 
 const emailInp = document.querySelector("#email");
 const passwordInp = document.querySelector("#password");
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
     const prsn = {
         email: emailInp.value.trim(),
         password: passwordInp.value.trim()
     };
 
-    previous.push(prsn);
-    localStorage.setItem("previous", JSON.stringify(previous));
+    // previous.push(prsn);
+    // localStorage.setItem("previous", JSON.stringify(previous));
 
-    const found = alluser.find(user =>
+    const foundedUser = alluser.find(user =>
         user.email === prsn.email && user.password === prsn.password
     );
 
-    if (found) {
+    // console.log("foundedUser:", foundedUser);
+    
+
+    if (foundedUser) {
         alert(" Welcome back!");
         emailInp.value = "";
         passwordInp.value = "";
+        localStorage.setItem("currentUser", JSON.stringify(foundedUser))
         window.location.href = "./welcome.html";
     } else {
         alert("Invalid login");
--- signup.js
+++ signup.js
@@ -1,31 +1,29 @@
-
-
-const inputs = document.querySelectorAll("input");  
+const inputs = document.querySelectorAll("input");
 const btn = document.querySelector("button");
 
-const alluser  = JSON.parse(localStorage.getItem("data")) || [];
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const alluser = JSON.parse(localStorage.getItem("data")) || [];
 
-btn.addEventListener("click", () => {
-    const user = {};
-    inputs.forEach(input => {
-        user[input.name] = input.value.trim();
-    });
+  const user = {};
+  inputs.forEach((input) => {
+    user[input.name] = input.value.trim();
+  });
 
-    if (!user.name || !user.email || !user.password) {
-        alert("❌ Please fill all fields.");
-        return;
-    }
+  if (!user.name || !user.email || !user.password) {
+    alert("❌ Please fill all fields.");
+    return;
+  }
 
-    const alreadyExist = alluser.find(usr => usr.email === user.email);
-    if (alreadyExist) {
-        alert("🫡 👆 Email already registered. Please login.");
-        return;
-    }
+  const alreadyExist = alluser.find((usr) => usr.email === user.email);
+  if (alreadyExist) {
+    alert("🫡 👆 Email already registered. Please login.");
+    return;
+  }
 
-    alluser.push(user);
-    localStorage.setItem("data", JSON.stringify(alluser));
+  alluser.push(user);
+  localStorage.setItem("data", JSON.stringify(alluser));
 
-    alert("Signup successful! Please login.");
-    window.location.href = "login.html";
+  alert("Signup successful! Please login.");
+  window.location.href = "./index.html";
 });
-
